1cf7e4cc40f724111f527e1ad715ffe856ab3402
Bul8a54ur <tairshechter1118@gmail.com>
2025-06-09 17:07:32 +0300

    draw move from cell to cell

--- cell.py
+++ cell.py
@@ -29,3 +29,15 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self.__win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        if undo:
+            color = "gray"
+        else:
+            color = "red"
+        selfX = (self.__x1 + self.__x2) / 2
+        selfY = (self.__y1 + self.__y2) / 2
+        otherX = (to_cell.__x1 + to_cell.__x2) / 2
+        otherY = (to_cell.__y1 + to_cell.__y2) / 2
+        line = Line(Point(selfX, selfY), Point(otherX, otherY))
+        self.__win.draw_line(line, color)
