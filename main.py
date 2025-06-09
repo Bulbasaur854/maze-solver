9761b1250811cb62ff0f3213d3e6702432bf94f8
Bul8a54ur <tairshechter1118@gmail.com>
2025-06-09 18:11:15 +0300

    maze class + drawing animation

--- main.py
+++ main.py
@@ -1,30 +1,10 @@
 from graphics import Window
-from cell import Cell
+from maze import Maze
 
 def main():
     win = Window(800, 600)
     
-    c1 = Cell(win)
-    c1.has_left_wall = False
-    c1.draw(50, 50, 100, 100)
-
-    c2 = Cell(win)
-    c2.has_right_wall = False
-    c2.draw(50, 200, 100, 250)
-
-    c3 = Cell(win)
-    c3.has_bottom_wall = False
-    c3.draw(225, 225, 250, 250)
-
-    c4 = Cell(win)
-    c4.has_top_wall = False
-    c4.draw(300, 300, 500, 500)
-
-    c4.draw_move(c3, False)
-    c4.draw_move(c1, False)
-    c2.draw_move(c3, True)
-    c1.draw_move(c2, True)
-
+    maze = Maze(10, 10, 29, 39, 20, 20, win)
 
     win.wait_for_close()    
 
---
+++ maze.py
@@ -0,0 +1,44 @@
+import time
+from cell import Cell
+
+class Maze:
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win,
+        ):
+        self.__x1 = x1
+        self.__y1 = y1
+        self.__num_rows = num_rows
+        self.__num_cols = num_cols
+        self.__cell_size_x = cell_size_x
+        self.__cell_size_y = cell_size_y
+        self.__win = win
+        self.__cells = []
+        self.__create_cells()
+
+    def __create_cells(self):
+        for i in range(self.__num_cols):
+            self.__cells.append([])
+            for j in range(self.__num_rows):
+                self.__cells[i].append(Cell(self.__win))
+                self.__draw_cell(i, j)
+
+    def __draw_cell(self, i, j):
+        offset_x = i * self.__cell_size_x
+        offset_y = j * self.__cell_size_y
+        pos_x1 = self.__x1 + offset_x
+        pos_y1 = self.__y1 + offset_y
+        pos_x2 = pos_x1 + self.__cell_size_x
+        pos_y2 = pos_y1 + self.__cell_size_y
+        self.__cells[i][j].draw(pos_x1, pos_y1, pos_x2, pos_y2)
+        self.__animate()
+        time.sleep(0.01)
+
+    def __animate(self):
+        self.__win.redraw()
