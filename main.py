3f8b4aec4f796e4b14726d3076033f8d61bb8c24
Bul8a54ur <tairshechter1118@gmail.com>
2025-06-09 16:38:56 +0300

    point & line classes + drawing line

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-from graphics import Window
+from graphics import Window, Line, Point
 
 def main():
     win = Window(800, 600)
-    win.wait_for_close()
+    
+    line1 = Line(Point(100,300), Point(700,300))
+    win.draw_line(line1, "magenta")
+
+
+    win.wait_for_close()    
 
 if __name__ == "__main__":
     main()
